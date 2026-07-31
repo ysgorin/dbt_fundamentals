550f6fea0e4445bb18ccf1323e2d674d8ee2c7cc
Jonathan Gorin <jgorin@hamilton.edu>
2026-07-31 09:56:53 -0400

    implemented sources

--- models/marts/finance/fct_customer_orders_practice.sql
+++ models/marts/finance/fct_customer_orders_practice.sql
@@ -1,4 +1,20 @@
-WITH paid_orders as (select Orders.ID as order_id,
+-- Import CTEs
+with raw_orders as (
+    select *
+    from {{ source('jaffle_shop', 'orders') }}
+),
+
+raw_customers as (
+    select *
+    from {{ source('jaffle_shop', 'customers') }}
+),
+
+raw_payments as (
+    select *
+    from {{ source('stripe', 'payment') }}
+),
+
+paid_orders as (select Orders.ID as order_id,
         Orders.USER_ID    as customer_id,
         Orders.ORDER_DATE AS order_placed_at,
             Orders.STATUS AS order_status,
@@ -6,20 +22,20 @@ WITH paid_orders as (select Orders.ID as order_id,
         p.payment_finalized_date,
         C.FIRST_NAME    as customer_first_name,
             C.LAST_NAME as customer_last_name
-    FROM dbt-tutorial.jaffle_shop.orders as Orders
+    FROM raw_orders as Orders
     left join (select ORDERID as order_id, max(CREATED) as payment_finalized_date, sum(AMOUNT) / 100.0 as total_amount_paid
-from dbt-tutorial.stripe.payment
+from raw_payments
 where STATUS <> 'fail'
 group by 1) p ON orders.ID = p.order_id
-left join dbt-tutorial.jaffle_shop.customers C on orders.USER_ID = C.ID ),
+left join raw_customers C on orders.USER_ID = C.ID ),
 
 customer_orders 
     as (select C.ID as customer_id
         , min(ORDER_DATE) as first_order_date
         , max(ORDER_DATE) as most_recent_order_date
         , count(ORDERS.ID) AS number_of_orders
-    from dbt-tutorial.jaffle_shop.customers C 
-    left join dbt-tutorial.jaffle_shop.orders as Orders
+    from raw_customers C 
+    left join raw_orders as Orders
     on orders.USER_ID = C.ID 
     group by 1)
 
