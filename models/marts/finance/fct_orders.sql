5d3417374b75addf0d9e5c8fd8c754c211def742
Jonathan Gorin <jgorin@hamilton.edu>
2026-08-07 09:29:03 -0400

    updated fact and dim models to reflect updated stage models

--- models/marts/finance/fct_orders.sql
+++ models/marts/finance/fct_orders.sql
@@ -11,7 +11,7 @@ payments as (
 order_payments as (
     select
         order_id,
-        sum (case when status = 'success' then amount end) as amount
+        sum (case when payment_status = 'success' then payment_amount end) as payment_amount
     from payments
     group by 1
 ),
@@ -21,7 +21,7 @@ final as (
         orders.order_id,
         orders.customer_id,
         orders.order_date,
-        coalesce(order_payments.amount, 0) as amount
+        coalesce(order_payments.amount, 0) as payment_amount
     from orders
     left join order_payments using (order_id)
 )
--- models/marts/marketing/dim_customers.sql
+++ models/marts/marketing/dim_customers.sql
@@ -14,7 +14,7 @@ customer_orders as (
         min(order_date) as first_order_date,
         max(order_date) as most_recent_order_date,
         count(order_id) as number_of_orders,
-        sum(amount) as lifetime_value
+        sum(payment_amount) as lifetime_value
     from orders
     group by 1
 ),
@@ -24,8 +24,8 @@ final as (
 
     select
         customers.customer_id,
-        customers.first_name,
-        customers.last_name,
+        customers.givenname,
+        customers.surname,
         customer_orders.first_order_date,
         customer_orders.most_recent_order_date,
         coalesce(customer_orders.number_of_orders, 0) as number_of_orders,
