0ae0a889e3ea73e333f32cabd988016c54f0b25c
Jonathan Gorin <jgorin@hamilton.edu>
2026-08-07 09:31:38 -0400

    fixed payment_amount identifier

--- models/marts/finance/fct_orders.sql
+++ models/marts/finance/fct_orders.sql
@@ -21,7 +21,7 @@ final as (
         orders.order_id,
         orders.customer_id,
         orders.order_date,
-        coalesce(order_payments.amount, 0) as payment_amount
+        coalesce(order_payments.payment_amount, 0) as payment_amount
     from orders
     left join order_payments using (order_id)
 )
