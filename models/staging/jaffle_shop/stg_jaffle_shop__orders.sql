d1cc46e3f4306333cfac52bc69a7b9b3d5e448b5
Jonathan Gorin <jgorin@hamilton.edu>
2026-08-07 09:25:44 -0400

    updated order stage model

--- models/staging/jaffle_shop/stg_jaffle_shop__orders.sql
+++ models/staging/jaffle_shop/stg_jaffle_shop__orders.sql
@@ -2,6 +2,10 @@ select
     id as order_id,
     user_id as customer_id,
     order_date,
-    status,
-    _etl_loaded_at
+    status as order_status,
+    _etl_loaded_at,
+    row_number() over (
+        partition by user_id
+        order by order_date, id
+    ) as user_order_seq
 from {{source('jaffle_shop','orders')}}
