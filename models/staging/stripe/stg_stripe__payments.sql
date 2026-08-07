9d2ea4d04eda5ff2d6c4abca361b7d7a48d6daa6
Jonathan Gorin <jgorin@hamilton.edu>
2026-08-07 09:24:01 -0400

    updated payments stage model

--- models/staging/stripe/stg_stripe__payments.sql
+++ models/staging/stripe/stg_stripe__payments.sql
@@ -2,9 +2,8 @@ select
     id as payment_id,
     orderid as order_id,
     paymentmethod as payment_method,
-    status,
-    -- amount is stored in cents, convert it to dollars
-    amount / 100 as amount,
-    created as created_at,
+    status as payment_status,
+    round(amount / 100.0, 2) as payment_amount,
+    created as payment_created_at,
     _batched_at
 from {{source('stripe','payment')}}
