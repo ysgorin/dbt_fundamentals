66768731247d691c87140e5ab2aeec8b8ad1ab9f
Jonathan Gorin <jgorin@hamilton.edu>
2026-08-07 09:48:12 -0400

    added order values joined CTE

--- models/marts/finance/fct_customer_orders.sql
+++ models/marts/finance/fct_customer_orders.sql
@@ -23,6 +23,16 @@ order_totals as (
     group by order_id, payment_status
 ),
 
+order_values_joined as (
+    select
+        orders.*,
+        order_totals.order_value_dollars,
+        order_totals.payment_status
+    from orders
+    left join order_totals
+    on orders.order_id = order_totals.order_id
+),
+
 -- Logical CTEs
 customer_order_history as (
     select
