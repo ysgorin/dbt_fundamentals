c20dd353357b8212aa65c054668499b03cc6bcaf
Jonathan Gorin <jgorin@hamilton.edu>
2026-08-07 09:37:32 -0400

    refactored staging models to use raw and transformed CTEs

--- models/staging/jaffle_shop/stg_jaffle_shop__customers.sql
+++ models/staging/jaffle_shop/stg_jaffle_shop__customers.sql
@@ -1,6 +1,16 @@
-select
-    id as customer_id,
-    first_name as givenname,
-    last_name as surname,
-    first_name || ' ' || last_name as full_name
-from {{source('jaffle_shop','customers')}}
+with raw as (
+    select *
+    from {{ source('jaffle_shop','customers') }}
+),
+
+transformed as (
+    select
+        id as customer_id,
+        first_name as givenname,
+        last_name as surname,
+        first_name || ' ' || last_name as full_name
+    from raw
+)
+
+select *
+from transformed
