16c6c100f5a8cefa0f4067110aa48e705b624eb1
Jonathan Gorin <jgorin@hamilton.edu>
2026-08-26 09:54:04 -0400

    incorporated new UDF into model

--- models/intermediate/int_customer_orders.sql
+++ models/intermediate/int_customer_orders.sql
@@ -64,8 +64,10 @@ customer_orders as (
 average_customer_order_totals as (
     select
         *,
-        customer_total_lifetime_value / customer_non_returned_order_count
-            as customer_avg_non_returned_order_value
+        {{ function('safe_divide') }}(
+            customer_total_lifetime_value,
+            customer_non_returned_order_count
+        ) as customer_avg_non_returned_order_value
     from customer_orders
 )
 
